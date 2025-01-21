4e90e3bbd02c4b56237f6a07496401bb69b0b69a
ahsan <ahsanbudi@gmail.com>
2025-01-22 05:42:21 +0700

    change navbar

--- resources/views/components/layouts/peserta/navbar.blade.php
+++ resources/views/components/layouts/peserta/navbar.blade.php
@@ -23,88 +23,16 @@
                     </label>
                 </li>
 
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="notificationDropdown" role="button"
-                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i data-feather="bell"></i>
-                        <div class="indicator">
-                            <div class="circle"></div>
-                        </div>
-                    </a>
-                    <div class="dropdown-menu p-0" aria-labelledby="notificationDropdown">
-                        <div class="px-3 py-2 d-flex align-items-center justify-content-between border-bottom">
-                            <p>6 New Notifications</p>
-                            <a href="javascript:;" class="text-secondary">Clear all</a>
-                        </div>
-                        <div class="p-1">
-                            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-                                <div
-                                    class="w-30px h-30px d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                                    <i class="icon-sm text-white" data-feather="gift"></i>
-                                </div>
-                                <div class="flex-grow-1 me-2">
-                                    <p>New Order Recieved</p>
-                                    <p class="fs-12px text-secondary">30 min ago</p>
-                                </div>
-                            </a>
-                            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-                                <div
-                                    class="w-30px h-30px d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                                    <i class="icon-sm text-white" data-feather="alert-circle"></i>
-                                </div>
-                                <div class="flex-grow-1 me-2">
-                                    <p>Server Limit Reached!</p>
-                                    <p class="fs-12px text-secondary">1 hrs ago</p>
-                                </div>
-                            </a>
-                            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-                                <div
-                                    class="w-30px h-30px d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                                    <img class="w-30px h-30px rounded-circle" src="https://via.placeholder.com/30x30"
-                                        alt="userr">
-                                </div>
-                                <div class="flex-grow-1 me-2">
-                                    <p>New customer registered</p>
-                                    <p class="fs-12px text-secondary">2 sec ago</p>
-                                </div>
-                            </a>
-                            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-                                <div
-                                    class="w-30px h-30px d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                                    <i class="icon-sm text-white" data-feather="layers"></i>
-                                </div>
-                                <div class="flex-grow-1 me-2">
-                                    <p>Apps are ready for update</p>
-                                    <p class="fs-12px text-secondary">5 hrs ago</p>
-                                </div>
-                            </a>
-                            <a href="javascript:;" class="dropdown-item d-flex align-items-center py-2">
-                                <div
-                                    class="w-30px h-30px d-flex align-items-center justify-content-center bg-primary rounded-circle me-3">
-                                    <i class="icon-sm text-white" data-feather="download"></i>
-                                </div>
-                                <div class="flex-grow-1 me-2">
-                                    <p>Download completed</p>
-                                    <p class="fs-12px text-secondary">6 hrs ago</p>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="px-3 py-2 d-flex align-items-center justify-content-center border-top">
-                            <a href="javascript:;">View all</a>
-                        </div>
-                    </div>
-                </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img class="w-30px h-30px ms-1 rounded-circle" src="https://via.placeholder.com/30x30"
+                        <img class="w-30px h-30px ms-1 rounded-circle" src="{{ asset('storage/' . auth()->user()->foto) }}"
                             alt="profile">
                     </a>
                     <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
                         <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
                             <div class="mb-3">
-                                <img class="w-80px h-80px rounded-circle" src="https://via.placeholder.com/80x80"
+                                <img class="w-80px h-80px rounded-circle" src="{{ asset('storage/' . auth()->user()->foto) }}"
                                     alt="">
                             </div>
                             <div class="text-center">
