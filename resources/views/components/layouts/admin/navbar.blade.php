1f4dc4551c3670309b2bc10dad6ae57d8c0a387d
ahsan <ahsanbudi@gmail.com>
2025-04-24 14:57:42 +0700

    add image profile at navbar admin and assessor

--- resources/views/components/layouts/admin/navbar.blade.php
+++ resources/views/components/layouts/admin/navbar.blade.php
@@ -35,12 +35,12 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="profileDropdown" role="button"
                         data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img class="w-30px h-30px ms-1 rounded-circle" src="" alt="profile">
+                        <img class="w-30px h-30px ms-1 rounded-circle" src="{{ asset('assets/images/admin.png') }}" alt="profile">
                     </a>
                     <div class="dropdown-menu p-0" aria-labelledby="profileDropdown">
                         <div class="d-flex flex-column align-items-center border-bottom px-5 py-3">
                             <div class="mb-3">
-                                <img class="w-80px h-80px rounded-circle" src="" alt="">
+                                <img class="w-80px h-80px rounded-circle" src="{{ asset('assets/images/admin.png') }}" alt="">
                             </div>
                             <div class="text-center">
                                 <p class="fs-16px fw-bolder">{{ auth()->guard('admin')->user()->nama }}</p>
