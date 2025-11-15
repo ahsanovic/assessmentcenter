28626ba0a77a723fd5505f8e8ede1fdb0e9ad003
ahsan <ahsanbudi@gmail.com>
2025-11-15 17:36:42 +0700

    pelanggaran tes pspk

---
+++ app/Livewire/Admin/PelanggaranTes/TesPspk/Index.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Livewire\Admin\PelanggaranTes\TesPspk;
+
+use App\Models\Event;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Url;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+#[Layout('components.layouts.admin.app', ['title' => 'Log Pelanggaran Tes'])]
+class Index extends Component
+{
+    use WithPagination;
+
+    public $selected_id;
+
+    #[Url(as: 'q')]
+    public ?string $search = '';
+
+    public function updatedSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function render()
+    {
+        $data = Event::with('logPelanggaran')
+            ->when($this->search, function ($query) {
+                $query->where('nama_event', 'like', '%' . $this->search . '%');
+            })
+            ->whereIn('metode_tes_id', [5,6])
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+
+        return view('livewire.admin.pelanggaran-tes.tes-pspk.index', compact('data'));
+    }
+
+    public function resetFilters()
+    {
+        $this->reset();
+        $this->resetPage();
+        $this->render();
+    }
+}
