a71ac22a2371ee594ac4181434cbb68238a44ce9
ahsan <ahsanbudi@gmail.com>
2025-11-15 17:40:13 +0700

    hide hasil tes potensi

--- resources/views/livewire/peserta/tes-potensi/hasil-nilai.blade.php
+++ resources/views/livewire/peserta/tes-potensi/hasil-nilai.blade.php
@@ -9,8 +9,12 @@
             <div class="card">
                 <div class="card-body d-flex justify-content-center text-center">
                     <div style="font-family: 'Helvetica Neue', sans-serif;">
-                        <div class="mb-2"><strong>Job Person Match (JPM)</strong> : {{ $nilai->jpm }} %</div>
-                        <div><strong>Kategori</strong> : {{ $nilai->kategori }}</div>
+                        {{-- <div class="mb-2"><strong>Job Person Match (JPM)</strong> : {{ $nilai->jpm }} %</div> --}}
+                        {{-- <div><strong>Kategori</strong> : {{ $nilai->kategori }}</div> --}}
+                        <h4 class="text-center mb-3">
+                            Terima kasih, <strong>{{ auth()->guard('peserta')->user()->nama }}</strong>!
+                        </h4>
+                        <h6>Anda telah menyelesaikan Tes {{ auth()->guard('peserta')->user()->event->metodeTes->metode_tes }} dengan baik dan penuh semangat!</h6>
                         <div class="row mt-4">
                             <form action="{{ route('peserta.logout') }}" method="post">
                                 @csrf
