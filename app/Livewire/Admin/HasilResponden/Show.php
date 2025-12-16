6b8e6ba1aa89ad3eca6e03c54ed457f7ad81f11e
ahsan <ahsanovic_7@yahoo.com>
2025-12-16 11:18:57 +0700

    fitur download hasil responden

--- app/Livewire/Admin/HasilResponden/Show.php
+++ app/Livewire/Admin/HasilResponden/Show.php
@@ -8,6 +8,7 @@ use Livewire\Attributes\Layout;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Rap2hpoutre\FastExcel\FastExcel;
 
 #[Layout('components.layouts.admin.app', ['title' => 'Hasil Responden'])]
 class Show extends Component
@@ -38,6 +39,46 @@ class Show extends Component
         $this->id_event = $idEvent;
         $this->event = Event::with('peserta')->findOrFail($this->id_event);
         $this->pertanyaan = Kuesioner::pluck('deskripsi', 'id')->toArray();
+    }
+
+    public function downloadExcel()
+    {
+        $peserta = $this->event->peserta()->with('jawabanResponden')->get();
+        $skorLabel = [
+            1 => 'Sangat Tidak Setuju',
+            2 => 'Tidak Setuju',
+            3 => 'Netral',
+            4 => 'Setuju',
+            5 => 'Sangat Setuju',
+        ];
+
+        $exportData = [];
+        $no = 1;
+
+        foreach ($peserta as $item) {
+            $jawaban = $item->jawabanResponden->first();
+            $kuesioner_ids = explode(',', $jawaban->kuesioner_id ?? '');
+            $skors = explode(',', $jawaban->skor ?? '');
+
+            $jawabanText = [];
+            foreach ($kuesioner_ids as $i => $id) {
+                $pertanyaan = $this->pertanyaan[$id] ?? '-';
+                $skor = $skors[$i] ?? '-';
+                $skorText = $skorLabel[$skor] ?? '-';
+                $jawabanText[] = "Pertanyaan: {$pertanyaan} | Skor: {$skorText}";
+            }
+
+            $exportData[] = [
+                'No' => $no++,
+                'Nama Peserta' => $item->nama,
+                'Jawaban Responden' => implode("\n", $jawabanText),
+                'Kritik & Saran' => $jawaban->jawaban_esai ?? '-',
+            ];
+        }
+
+        $filename = 'jawaban-responden-' . str_replace(' ', '-', strtolower($this->event->name ?? 'event')) . '-' . date('Y-m-d') . '.xlsx';
+
+        return (new FastExcel(collect($exportData)))->download($filename);
     }
 
     public function render()
