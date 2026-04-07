54fc75e94f2ec2d53ed805fc0beac7cdf5ff5bbc
ahsan <ahsanbudi@gmail.com>
2026-04-07 13:09:07 +0700

    fix bug icon

--- resources/views/components/portofolio/alert-info.blade.php
+++ resources/views/components/portofolio/alert-info.blade.php
@@ -11,7 +11,7 @@
         <div class="card border-0 shadow-sm border-start border-4 border-{{ $color }}">
             <div class="card-body p-4">
                 <div class="d-flex align-items-start">
-                    <div class="me-3">
+                    <div class="me-3" wire:ignore>
                         <i class="text-{{ $color }}" data-feather="{{ $icon }}" style="width: 24px; height: 24px;"></i>
                     </div>
                     <div>
--- resources/views/components/portofolio/tab-nav.blade.php
+++ resources/views/components/portofolio/tab-nav.blade.php
@@ -1,5 +1,5 @@
 <div class="col-4 col-md-2 pe-0">
-    <div class="nav nav-tabs nav-tabs-vertical" id="v-tab" role="tablist" aria-orientation="vertical">
+    <div class="nav nav-tabs nav-tabs-vertical" id="v-tab" role="tablist" aria-orientation="vertical" wire:ignore>
         @foreach ($nav as $item)
             <a
                 class="nav-link d-flex align-items-center {{ $item['active'] }}"
@@ -8,7 +8,7 @@
                 role="tab"
                 wire:navigate
             >
-                <span>
+                <span wire:ignore>
                     <i data-feather="{{ $item['icon'] ?? 'circle' }}" class="text-{{ $item['color'] ?? 'secondary' }} me-2" style="width: 20px; height: 20px;"></i> 
                     {{ $item['title'] }}
                 </span>
